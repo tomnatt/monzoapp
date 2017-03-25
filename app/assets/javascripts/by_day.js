12743e5f4b3b801180941ee1919ef884585e94c8
tomnatt <github@tomnatt.com>
2017-03-25 19:55:06 +0000

    Working with hard-coded data

--- app/assets/javascripts/by_day.js
+++ app/assets/javascripts/by_day.js
@@ -1,12 +1,31 @@
 var ctx = document.getElementById("myChart");
 
+/*
+ * Test data:
+ * £30 1/2/17
+ * £15 2/2/17
+ * £45 3/2/17
+ * £35 4/2/17
+ */
+
 var moneyset = {
   label: 'Money',
-  data: $.ajax({
-    url: '/data/byday',
-    type: 'jsonp'
-  })
+  data: [
+    { x: '2017-02-01', y: 50 },
+    { x: '2017-02-01', y: 30 },
+    { x: '2017-02-02', y: 15 },
+    { x: '2017-02-03', y: 45 },
+    { x: '2017-02-04', y: 35 },
+  ]
 }
+
+// var moneyset = {
+//   label: 'Money',
+//   data: $.ajax({
+//     url: 'http://localhost:3000/data/byday',
+//     type: 'jsonp'
+//   })
+// }
 
 // And for a line chart
 var lineChart = new Chart(ctx, {
