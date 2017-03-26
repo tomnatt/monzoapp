43422b7ebe8ea86c59dd04f5a01d29d932ca3e5f
tomnatt <github@tomnatt.com>
2017-03-26 16:31:07 +0100

    Add tests and retain a single API object

--- lib/transaction_service.rb
+++ lib/transaction_service.rb
@@ -1,12 +1,16 @@
 module TransactionService
   # TODO: don't create a new one of these every time
   def self.monzo_api
-    Mondo::Client.new(
+    return @api if @api.present?
+    @api = Mondo::Client.new(
       token: ENV['MONZO_ACCESS_TOKEN'],
       account_id: ENV['MONZO_ACCOUNT_ID'] # recommended, but optional. If not set, the client will fetch it from the API
     )
+
+    @api
   end
 
+  # TODO: cache this response
   def self.all_transactions
     monzo_api.transactions
   end
