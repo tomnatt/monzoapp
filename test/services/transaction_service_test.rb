dd37876f6e2159499f9808f350c143bcf7c6e947
tomnatt <github@tomnatt.com>
2017-04-01 18:45:27 +0100

    First pass at stubbing Monzo API

--- test/services/transaction_service_test.rb
+++ test/services/transaction_service_test.rb
@@ -3,6 +3,47 @@ require 'transaction_service'
 
 # Test TransactionService
 class TransactionServiceTest < ActiveSupport::TestCase
+  def setup
+    stub_request(:get, 'https://api.getmondo.co.uk/transactions')
+      .with(query: hash_including(), # accept any query parameters, including account_id
+            headers: { 'Accept' => 'application/json',
+                       'User-Agent' => /mondo-ruby/ })
+      .to_return(status: 200, body: %Q(
+        {
+    "transactions": [
+        {
+            "account_balance": 13013,
+            "amount": -510,
+            "created": "2015-08-22T12:20:18Z",
+            "currency": "GBP",
+            "description": "THE DE BEAUVOIR DELI C LONDON        GBR",
+            "id": "tx_00008zIcpb1TB4yeIFXMzx",
+            "merchant": "merch_00008zIcpbAKe8shBxXUtl",
+            "metadata": {},
+            "notes": "Salmon sandwich 🍞",
+            "is_load": false,
+            "settled": "2015-08-23T12:20:18Z",
+            "category": "eating_out"
+        },
+        {
+            "account_balance": 12334,
+            "amount": -679,
+            "created": "2015-08-23T16:15:03Z",
+            "currency": "GBP",
+            "description": "VUE BSL LTD            ISLINGTON     GBR",
+            "id": "tx_00008zL2INM3xZ41THuRF3",
+            "merchant": "merch_00008z6uFVhVBcaZzSQwCX",
+            "metadata": {},
+            "notes": "",
+            "is_load": false,
+            "settled": "2015-08-24T16:15:03Z",
+            "category": "eating_out"
+        }
+    ]
+}
+), headers: {})
+  end
+
   # Check we are getting a proper client object
   def test_monzo_api_object
     assert_kind_of Mondo::Client, TransactionService.monzo_api,
@@ -20,8 +61,10 @@ class TransactionServiceTest < ActiveSupport::TestCase
     transactions = TransactionService.all_transactions
     assert transactions, 'Should not return nothing'
     refute transactions.empty?, 'There should be a transaction'
+    assert_equal 2, transactions.length, 'There should be two transactions'
     assert_kind_of Mondo::Transaction, transactions.first,
                    'Response should be an array of Mondo::Transaction'
+    assert_equal Money.new(-510, "GBP"), transactions.first.amount
   end
 
   # Cache the transactions response
