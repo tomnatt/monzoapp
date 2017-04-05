eb2bd52cd184d2d8c14ccad19039dac5983c87bf
tomnatt <github@tomnatt.com>
2017-04-05 20:40:10 +0100

    Update data controller to use new service object

--- app/controllers/data/data_controller.rb
+++ app/controllers/data/data_controller.rb
@@ -1,17 +1,8 @@
-# require 'chart_j_s_service'
+require 'chart_j_s_service'
 
 # Serve data from service objects as JSON
 class Data::DataController < ApplicationController
   def by_day
-    balance_by_day = []
-
-    # Get the final balance on each day
-    TransactionService.all_transactions.each do |transaction|
-      date = transaction.created.strftime('%Y-%m-%d') # eg 2017-02-23
-      balance = transaction.account_balance.cents / 100 # want this in pounds not pence
-      balance_by_day << { x: date, y: balance }
-    end
-
-    render json: balance_by_day
+    render json: ChartJSService.balance_by_day_for_scatter_graph
   end
 end
