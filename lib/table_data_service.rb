48b46f36d45c03d3d5e504f88c8479dc82172703
tomnatt <github@tomnatt.com>
2017-04-27 20:39:52 +0100

    Rename methods and update test data
    A transaction on a Monday was incorrectly being assigned to the previous week.

--- lib/table_data_service.rb
+++ lib/table_data_service.rb
@@ -2,21 +2,26 @@ require 'transaction_service'
 
 # Methods for formatting data for use in tables
 module TableDataService
-  def self.expenditure_by_week
-    expenditure_by_week = []
+  def self.transactions_by_week
+    transactions_by_week = []
 
     # Collect transactions by week
     TransactionService.all_transactions.each do |transaction|
-      monday_before_transaction = Chronic.parse('last monday', now: transaction.created).to_date
+      monday_before_transaction = last_monday(transaction.created)
 
       # If this week is already in the array add the amount, else create a new entry
-      if expenditure_by_week.map { |x| x[:week] }.include?(monday_before_transaction)
-        expenditure_by_week.last[:expenditure] += transaction.amount
+      if transactions_by_week.map { |x| x[:week] }.include?(monday_before_transaction)
+        transactions_by_week.last[:transactions] += transaction.amount
       else
-        expenditure_by_week << { week: monday_before_transaction, expenditure: transaction.amount }
+        transactions_by_week << { week: monday_before_transaction, transactions: transaction.amount }
       end
     end
 
-    expenditure_by_week
+    transactions_by_week
+  end
+
+  def self.last_monday(transaction_date)
+    return transaction_date.to_date if transaction_date.monday?
+    Chronic.parse('last monday', now: transaction_date).to_date
   end
 end
--- test/services/table_data_service_test.rb
+++ test/services/table_data_service_test.rb
@@ -12,18 +12,18 @@ class TableDataServiceTest < MockedTest
     assert_equal 4, @transactions.length, 'There should be four transactions'
   end
 
-  # Test we are getting data of the form [{ week: DateTime, expenditure: Money }, ...]
-  def test_expenditure_by_week
-    assert_equal 2, TableDataService.expenditure_by_week.length, 'There should be two weeks of data'
+  # Test we are getting data of the form [{ week: DateTime, transactions: Money }, ...]
+  def test_transactions_by_week
+    assert_equal 2, TableDataService.transactions_by_week.length, 'There should be two weeks of data'
 
-    assert_equal Chronic.parse('2015-08-10').to_date, TableDataService.expenditure_by_week.first[:week],
+    assert_equal Chronic.parse('2015-08-10').to_date, TableDataService.transactions_by_week.first[:week],
                  'First week should have started 2015-08-10'
-    assert_equal Money.new(-1732, 'GBP'), TableDataService.expenditure_by_week.first[:expenditure],
+    assert_equal Money.new(-1732, 'GBP'), TableDataService.transactions_by_week.first[:transactions],
                  'Should have spent 1732 in the first week'
 
-    assert_equal Chronic.parse('2015-08-17').to_date, TableDataService.expenditure_by_week.last[:week],
+    assert_equal Chronic.parse('2015-08-17').to_date, TableDataService.transactions_by_week.last[:week],
                  'Second week should have started 2015-08-17'
-    assert_equal Money.new(-1189, 'GBP'), TableDataService.expenditure_by_week.last[:expenditure],
+    assert_equal Money.new(-1189, 'GBP'), TableDataService.transactions_by_week.last[:transactions],
                  'Should have spent 1189 in the second week'
   end
 end
