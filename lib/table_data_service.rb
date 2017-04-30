a049d5c773d5c3af0c23bc853bc6967c2de12c2c
tomnatt <github@tomnatt.com>
2017-04-30 17:09:22 +0100

    Added expenditure and income to weekly summary
    Also udpate test data and tests

--- lib/table_data_service.rb
+++ lib/table_data_service.rb
@@ -12,8 +12,24 @@ module TableDataService
       # If this week is already in the array add the amount, else create a new entry
       if transactions_by_week.map { |x| x[:week] }.include?(monday_before_transaction)
         transactions_by_week.last[:transactions] += transaction.amount
+        if transaction_is_income?(transaction)
+          transactions_by_week.last[:income] += transaction.amount
+        else
+          transactions_by_week.last[:expenditure] += transaction.amount
+        end
+
       else
-        transactions_by_week << { week: monday_before_transaction, transactions: transaction.amount }
+        transactions_by_week << if transaction_is_income?(transaction)
+                                  { week: monday_before_transaction,
+                                    transactions: transaction.amount,
+                                    income: transaction.amount,
+                                    expenditure: Money.new(0, 'GBP') }
+                                else
+                                  { week: monday_before_transaction,
+                                    transactions: transaction.amount,
+                                    income: Money.new(0, 'GBP'),
+                                    expenditure: transaction.amount }
+                                end
       end
     end
 
@@ -23,5 +39,9 @@ module TableDataService
   def self.last_monday(transaction_date)
     return transaction_date.to_date if transaction_date.monday?
     Chronic.parse('last monday', now: transaction_date).to_date
+  end
+
+  def self.transaction_is_income?(transaction)
+    transaction.metadata['is_topup'] == 'true'
   end
 end
